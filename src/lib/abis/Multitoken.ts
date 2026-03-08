2cad3acd6bf36a43bf26fa1fe92ab4a6f6fcd30d
Agbor Franklen <ayukfrank2@gmail.com>
2026-03-08 07:24:16 +0100

    new

--- src/lib/abis/Multitoken.ts
+++ src/lib/abis/Multitoken.ts
@@ -1,62 +1,64 @@
 export const MULTITOKEN_CONTRACT_ADDRESS =
-  "0xb0c0fe3a0ec9cd2d5d59efb54e8d27fc03aeadeb" as const;
+  "0xe092495b47Cb64D78dED5e7cdFb7b7776834A676" as const;
 
 export const MULTITOKEN_ABI = [
-  { inputs: [], stateMutability: "nonpayable", type: "constructor" },
-  {
-    inputs: [{ internalType: "address[]", name: "_tokens", type: "address[]" }],
-    name: "addTokens",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [{ internalType: "address", name: "proxy", type: "address" }],
-    name: "approveProxy",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "listTokens",
-    outputs: [{ internalType: "address[]", name: "", type: "address[]" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "missingApprovals",
-    outputs: [{ internalType: "address[]", name: "", type: "address[]" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "owner",
-    outputs: [{ internalType: "address", name: "", type: "address" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [{ internalType: "address", name: "proxy", type: "address" }],
-    name: "revokeProxy",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-    name: "tokens",
-    outputs: [{ internalType: "address", name: "", type: "address" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [{ internalType: "address", name: "recipient", type: "address" }],
-    name: "transferAll",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
+    { inputs: [], stateMutability: "nonpayable", type: "constructor" },
+    {
+      inputs: [
+        { internalType: "address[]", name: "_tokens", type: "address[]" },
+      ],
+      name: "addTokens",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "proxy", type: "address" }],
+      name: "approveProxy",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "listTokens",
+      outputs: [{ internalType: "address[]", name: "", type: "address[]" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "missingApprovals",
+      outputs: [{ internalType: "address[]", name: "", type: "address[]" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "owner",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "proxy", type: "address" }],
+      name: "revokeProxy",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+      name: "tokens",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [{ internalType: "address", name: "recipient", type: "address" }],
+      name: "transferAll",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
 ] as const;
